5c92c6fd475021b95569a677940ae188651b57e7
ali asgar <67861700+joancena@users.noreply.github.com>
2020-12-03 23:51:09 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      {/* header */}
+      {/* side bar */}
+    </div>
+  );
+}
+
+export default App;
